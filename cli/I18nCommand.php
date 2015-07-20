83b3dd83c2906bd6e020b5ad4feabd61595816e0
David G. Shannon <gooster.snesi@gmail.com>
2015-07-20 14:54:35 +0200

    Renamed I18Commands to I18Command

--- cli/I18nCommand.php
+++ cli/I18nCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class I18nCommands extends Command
+class I18nCommand extends Command
 {
     protected function configure()
     {
